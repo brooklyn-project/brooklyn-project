a067a13c23c0d27989a7ee7717b69eec8542e0ac
Harrison Jin <harrison.jin@utexas.edu>
2023-02-13 12:43:29 -0600

    remove upload script and add auto white balance option

--- experimental/record_video.py
+++ experimental/record_video.py
@@ -6,12 +6,14 @@ VIDEO_DIR = '/home/pi/brooklyn-project/experimental/videos/'
 
 parser = argparse.ArgumentParser(description='Record a video from the Raspberry Pi camera')
 parser.add_argument('--preview', action='store_true', help='Show a preview of the camera')
+parser.add_argument('--awb_mode', type=str, help='Set the auto white balance mode. Potential values: auto, sunlight, cloudy. Defaults to auto', default='auto')
 parser.add_argument('duration', type=int, help='Duration of the video in seconds')
 
 args = parser.parse_args()
 timestamp = time.strftime("%m-%d-%H%M%S")
 
 with picamera.PiCamera(sensor_mode=4) as camera:
+    camera.awb_mode = args.awb_mode
     camera.resolution = (1296, 972)
     camera.framerate = 42
     if args.preview:
