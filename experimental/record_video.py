c8439739c732896223e5f42c26b7b161f7747071
Harrison Jin <33787152+harrijin@users.noreply.github.com>
2023-02-28 16:54:45 -0600

    bugfix

--- experimental/record_video.py
+++ experimental/record_video.py
@@ -3,16 +3,7 @@ import picamera
 import time
 import signal
 
-timestamp = time.strftime("%m-%d-%H%M%S")
-stream = picamera.PiCameraCircularIO(camera, seconds=3600)
 VIDEO_DIR = '/home/pi/brooklyn-project/experimental/videos/'
-
-def save_recording(sig, frame):
-    print("saving video")
-    stream.copy_to(VIDEO_DIR + f'{timestamp}.h264')
-    sys.exit(0)
-    
-signal.signal(signal.SIGINT, save_recording)
 
 parser = argparse.ArgumentParser(description='Record a video from the Raspberry Pi camera')
 parser.add_argument('--preview', action='store_true', help='Show a preview of the camera')
@@ -21,7 +12,17 @@ parser.add_argument('duration', type=int, help='Duration of the video in seconds
 
 args = parser.parse_args()
 
+timestamp = time.strftime("%m-%d-%H%M%S")
+
 with picamera.PiCamera(sensor_mode=4) as camera:
+    stream = picamera.PiCameraCircularIO(camera, seconds=3600)
+
+    def save_recording(sig, frame):
+        print("saving video")
+        stream.copy_to(VIDEO_DIR + f'{timestamp}.h264')
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, save_recording)
     camera.awb_mode = args.awb_mode
     camera.resolution = (1296, 972)
     camera.framerate = 42
