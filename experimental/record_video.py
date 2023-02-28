18b649a82aef59b8da5f9afc27163b8331b9c998
Harrison Jin <33787152+harrijin@users.noreply.github.com>
2023-02-28 16:53:17 -0600

    Add SIGINT handler and switch to circular buffer

--- experimental/record_video.py
+++ experimental/record_video.py
@@ -1,8 +1,18 @@
 import argparse
 import picamera
 import time
+import signal
 
+timestamp = time.strftime("%m-%d-%H%M%S")
+stream = picamera.PiCameraCircularIO(camera, seconds=3600)
 VIDEO_DIR = '/home/pi/brooklyn-project/experimental/videos/'
+
+def save_recording(sig, frame):
+    print("saving video")
+    stream.copy_to(VIDEO_DIR + f'{timestamp}.h264')
+    sys.exit(0)
+    
+signal.signal(signal.SIGINT, save_recording)
 
 parser = argparse.ArgumentParser(description='Record a video from the Raspberry Pi camera')
 parser.add_argument('--preview', action='store_true', help='Show a preview of the camera')
@@ -10,7 +20,6 @@ parser.add_argument('--awb_mode', type=str, help='Set the auto white balance mod
 parser.add_argument('duration', type=int, help='Duration of the video in seconds')
 
 args = parser.parse_args()
-timestamp = time.strftime("%m-%d-%H%M%S")
 
 with picamera.PiCamera(sensor_mode=4) as camera:
     camera.awb_mode = args.awb_mode
@@ -19,8 +28,12 @@ with picamera.PiCamera(sensor_mode=4) as camera:
     if args.preview:
         camera.start_preview()
     print("STARTING RECORDING")
-    camera.start_recording(VIDEO_DIR + f'{timestamp}.h264')
-    camera.wait_recording(args.duration)
-    print("RECORDING FINISHED")
-    camera.stop_recording()
+    camera.start_recording(stream, format='h264')
+
+    try:
+        while True:
+            camera.wait_recording(1)
+    except:
+        print("STOPPING RECORDING")
+        camera.stop_recording()
 
