488a78ef61805c04f147ac603b7c254874cbd6a6
Abdon <averdejo02@utexas.edu>
2023-03-29 12:17:44 -0500

    Using different functions to test the functions

--- target-recognition/main.cpp
+++ target-recognition/main.cpp
@@ -2,13 +2,15 @@
 
 int main(int argc, char** argv) {
 
-	std::vector<std::string> pictureArray{ "./resources/noTarget.jpg", "./resources/oneTarget.jpg", "./resources/targetFace.jpg", "./resources/twoTargets.jpg", "./resources/targetFaceSmile.jpg" };
-	std::string photoPath = "./resources/targetFace.jpg";
-	std::string videoPath = "./resources/FlightTestFootage.mp4";
-	std::vector<std::string> videoArray{"./resources/video1.mp4", "./resources/video2.mp4", "./resources/video3.mp4", "./resources/video4.mp4", "./resources/video5.mp4"};
+	std::vector<std::string> pictureArray{ "./resources/noTarget.jpg", "./resources/oneTarget.jpg", "./resources/twoTargets.jpg", \
+		"./resources/targetFaceSad.jpg", "./resources/targetFaceSmile.jpg", \
+		"./resources/critical_and_one_toi.jpg", "./resources/critical_and_two_toi.jpg"};
+	std::string photoPath = "./resources/critical_and_one_toi.jpg";
+	std::string videoPath = "./resources/search_phase.mp4";
+	std::vector<std::string> videoArray {"./resources/search_phase.mp4"};
 	Camera camera = Camera();
 	bool debug = true;
-	bool showImage = false;
+	bool showImage = true;
 
 	////Test the analyze_and_draw from an inputted video
 	//for (std::string video : videoArray) {
@@ -20,27 +22,37 @@ int main(int argc, char** argv) {
 	//	camera.analyze_and_draw_video(video, debug);
 	//}
 
-	////Play aroung with the camera input
-	//camera.analyze_and_draw_camera(showImage);
+	////Try to read in the video that was clipped
+	//camera.analyze_and_draw_video(videoPath);
 
-	//Looping through every image of the array
-	for (std::string image : pictureArray) {
-		//Create a mat image
-		cv::Mat matImage = camera.createImgMat(image);
+	////Play aroung with the camera input on my laptop
+	//camera.analyze_and_draw_camera();
 
-		if (debug) {
-			//Print out a prompt to the user
-			std::cout << "The image: " << image << " has the following toi and critical targets:\n";
-		}
+	////Looping through every image of the array
+	//for (std::string image : pictureArray) {
+	//	//Create a mat image
+	//	cv::Mat matImage = camera.createImgMat(image);
 
-		//Input that into teh "analyze_and_draw" function
-		camera.analyse_and_draw(matImage, debug, showImage);
+	//	if (debug) {
+	//		//Print out a prompt to the user
+	//		std::cout << "The image: " << image << " has the following toi and critical targets:\n";
+	//	}
 
-	}
+	//	//Input that into teh "analyze_and_draw" function
+	//	camera.analyse_and_draw(matImage, nullptr, nullptr, debug, showImage);
+
+	//}
+
+	////This to test analyze_and_draw() for one image
+	//cv::Mat image = camera.createImgMat(photoPath);
+	//camera.analyse_and_draw(image, nullptr, nullptr, debug, showImage);
+
+	////This is to calibrate the camera
+	//camera.calibrateFromPhoto(photoPath);
+
 
 	////This is needed to test with my function
 	//camera.resetThresholdVals();
 	//camera.testFunction();
-
 	return 0;
 }
