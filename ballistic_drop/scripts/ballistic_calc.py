5f465548b1fe01d15499e29b8db162499b8753c1
Eric <ericrwang2011@gmail.com>
2023-04-04 17:05:57 -0500

    ballistics update

--- ballistic_drop/scripts/ballistic_calc.py
+++ ballistic_drop/scripts/ballistic_calc.py
@@ -2,12 +2,13 @@
 import rospy
 from std_msgs.msg import String, Float64MultiArray
 import numpy as np
+import math
 
 # GLOBAL VARIABLES (CONSTANT THROUGHOUT FLIGHT)
-C_D = 0 
-RHO = 0 
-A = 1 # area exposed to direction of drop
-M = 2 # payload mass
+C_D = 0.05 # Missing
+RHO = 1.293 # Missing
+A = 0.05 # area exposed to direction of drop
+M = 0.1133981 # payload mass (kg)
 G = 9.81 # gravity constant 
 P = 1/7 # exponential parameter from wind update
 
@@ -19,36 +20,49 @@ def wind_Update(w_1, z_1, z_2):
     return w_2
 
 # Solving the ODE 
-def solve_ODE(v_x, v_y, v_z, w_x, w_y, w_z):
+def solve_ODE(v_x, v_y, v_z, w_x, w_y, w_z, x_disp, y_disp, z_loc):
 
     # Update wind estimate and airspeed V_r 
     v_r = np.sqrt((v_x - w_x)**2 + (v_y - w_y)**2 + (v_z - w_z)**2)
 
     # Solving ODE
-    h = 0.5 # Set size
+    h = 0.05 # Set size
+
+    x_disp += v_x*h
+    y_disp += v_y*h
+    z_loc -= v_z*h
 
     v_x_dot = (-C_D*RHO*A)/(2*M)*(v_x - w_x)*v_r
     v_y_dot = (-C_D*RHO*A)/(2*M)*(v_y - w_y)*v_r
     v_z_dot = G - (C_D*RHO*A)/(2*M)*(v_z - w_z)*v_r
 
-    x, y, z = 0, 0, 0
-    return x, y, z, v_x, v_y, v_z
+    v_x += v_x_dot*h
+    v_y += v_y_dot*h
+    v_z += v_z_dot*h
 
+    return x_disp, y_disp, z_loc, v_x, v_y, v_z
 
+# Determine location of drop for payload
 def bal_Calc(target_lat, target_long, V, true_course, z_loc, w_x, w_y, w_z):
     # Drop altitude, initial z value
     z_curr = z_loc
     w_x_curr, w_y_curr, w_z_curr = w_x, w_y, w_z
 
+    # x and y displacement
+    x_disp, y_disp =  0, 0
+
     # Aircraft speed component
-    v_x = V*np.cos(true_course)
-    v_y = V*np.sin(true_course)
+    v_x = V*np.sin(true_course*math.pi/180)
+    v_y = V*np.cos(true_course*math.pi/180)
     v_z = 0
 
     # First step 
     z_prev = z_curr
+    x = []
+    y = []
+    z = []
 
-    x_curr, y_curr, z_curr, v_x, v_y, v_z = solve_ODE(v_x, v_y, v_z, w_x_curr, w_y_curr, w_z_curr)
+    x_disp, y_disp, z_curr, v_x, v_y, v_z = solve_ODE(v_x, v_y, v_z, w_x_curr, w_y_curr, w_z_curr, x_disp, y_disp, z_curr)
 
     while z_curr > 0:
         # Get wind update
@@ -56,14 +70,21 @@ def bal_Calc(target_lat, target_long, V, true_course, z_loc, w_x, w_y, w_z):
         w_y_curr = wind_Update(w_y_curr, z_prev, z_curr)
         w_z_curr = wind_Update(w_z_curr, z_prev, z_curr)
 
+        # print(w_x_curr, w_y_curr, w_z_curr)
+
         # Set previous z coordinate to previous variable
         z_prev = z_curr
 
         # Solve ODE for new x,y,z coordinate and velocity
-        x_disp, y_disp, z_curr, v_x, v_y, v_z = solve_ODE(v_x, v_y, v_z, w_x_curr, w_y_curr, w_z_curr)
+        x_disp, y_disp, z_curr, v_x, v_y, v_z = solve_ODE(v_x, v_y, v_z, w_x_curr, w_y_curr, w_z_curr, x_disp, y_disp, z_curr)
+        print(x_disp, y_disp, z_curr, v_x, v_y, v_z)
+        x.append(x_disp)
+        y.append(y_disp)
+        z.append(z_curr)
         
     # Release coordinates are equal to the target coordinates, shifted 
     # − x m in the north direction and − y m in the east direction
+    return x_disp, y_disp, z_curr
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + " ~ %s", data.data)
@@ -77,9 +98,11 @@ if __name__ == '__main__':
     target_lat = 0
     target_long = 0
     V = 40
-    true_course = 0
+    true_course = 20
     z_loc = 400
-    w_x, w_y, w_z = 1,1,0
+    w_x, w_y, w_z = 0,0,0
 
-    bal_Calc(target_lat, target_long, V, true_course, z_loc, w_x, w_y, w_z)
+    x_disp, y_disp, z_curr = bal_Calc(target_lat, target_long, V, true_course, z_loc, w_x, w_y, w_z)
+    # print(x_disp, y_disp, z_curr)
     # predict_drop()
+
